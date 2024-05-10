784fe360770bb33847a4b0d08dc0278b2f611bca
towhid046 <towhidmorol46@gmail.com>
2024-05-10 09:37:55 +0600

    connect mongodb and added a POST method to save serviceinfo

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 5000;
 
 // middleware:
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send('FixFast is running...');
+  res.send("FixFast is running...");
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q1nysvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const serviceCollection = client.db("fixFastDB").collection("services");
+
+    app.post("/add-service", async (req, res) => {
+      const serviceInfo = req.body;
+      const result = await serviceCollection.insertOne(serviceInfo);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`FixFast listening at http://localhost:${port}`);
+  console.log(`FixFast is running at http://localhost:${port}`);
 });
