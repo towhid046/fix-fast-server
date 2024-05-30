63b3b57895bbd3448137e0af0f5a4fd110c33039
towhid046 <towhidmorol46@gmail.com>
2024-05-30 09:20:02 +0600

    added new api to cancle booked service

--- index.js
+++ index.js
@@ -234,6 +234,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a booked service by Id:
+    app.delete("/booked-service/:id", async (req, res) => {
+      const id = req.params?.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookedServiceCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
